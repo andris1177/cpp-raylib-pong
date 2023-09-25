1cabf377a27f58fe6ddda7029a127c9a16e2721b
andris1177 <laszloandris1@gmail.com>
2023-09-25 20:47:08 +0200

    update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-package_names = "cmake" "raylib"
 
-linux()
-{
+raylib_location="/usr/local/lib/libraylib.a"
+
+linux() {
     # debian based apt
     if [ -f "/etc/debian_version" ]; then
         debian
@@ -12,70 +12,30 @@ linux()
     if [ -f "/etc/redhat-release" ]; then
         if [ -f "/usr/bin/yum" ]; then
             redHatYum
-
         elif [ -f "/usr/bin/dnf" ]; then
             redHatRpm
         fi
     fi
 
-    # Carch based pacman
+    # Arch based pacman
     if [ -f "/etc/arch-release" ]; then
         arch
     fi
+
     unixBuild
 }
 
-debian()
-{
-    for package_name in "${package_names[@]}"; do
-        if dpkg -l | grep -w "$package_name" > /dev/null; then
-            echo "$package_name already installed."
-        else
-            echo "$package_name is not installed."
-            sudo apt-get update
-            sudo apt-get install -y "$package_name"
-        fi
-    done
+debian() {
+    sudo apt-get install -y libasound2-dev libx11-dev libxrandr-dev libxi-dev libgl1-mesa-dev libglu1-mesa-dev libxcursor-dev libxinerama-dev cmake
+    installRaylib
 }
 
-redHatYum()
-{
-    for package_name in "${package_names[@]}"; do
-        if yum list installed "$package_name" > /dev/null; then
-            echo "$package_name already installed."
-        else
-            echo "$package_name is not installed."
-            sudo yum install -y "$package_name"
-        fi
-    done
+arch() {
+    sudo pacman -S --noconfirm alsa-lib mesa libx11 libxrandr libxi libxcursor libxinerama cmake
+    installRaylib
 }
 
-redHatRpm()
-{
-    for package_name in "${package_names[@]}"; do
-        if rpm -q "$package_name" > /dev/null; then
-            echo "$package_name already installed."
-        else
-            echo "$package_name is not installed."
-            sudo dnf install -y "$package_name"
-        fi
-    done
-}
-
-arch()
-{
-     for package_name in "${package_names[@]}"; do
-        if pacman -Qs "$package_name" > /dev/null; then
-            echo "$package_name already installed."
-        else
-            echo "$package_name is not installed."
-            sudo pacman -Sy --noconfirm $package_name
-        fi
-    done
-}
-
-macos()
-{
+macos() {
     # Install Homebrew, which is an open source package manager for macOS.
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     brew install cmake
@@ -84,39 +44,50 @@ macos()
     unixBuild
 }
 
-unixBuild()
-{
-    #check if build folder exisit
+installRaylib() {
+    if [ -f "$raylib_location" ]; then
+        echo "raylib is installed"
+    else
+        git clone https://github.com/raysan5/raylib.git raylib
+        cd raylib/src/
+        make PLATFORM=PLATFORM_DESKTOP
+        sudo make install
+        cd ../..
+        rm -r raylib/ -y
+    fi
+}
+
+unixBuild() {
+    # Check if the build folder exists
     if [ -d "./build" ]; then
-        echo "the folder is already exist"
+        echo "the folder already exists"
         build
     else
-        echo "folder doesn't exist creating..."
+        echo "folder doesn't exist, creating..."
         mkdir build
         build
     fi
 }
 
-build()
-{
-    cd build 
+build() {
+    cd build
     cmake ..
     if [ $? -ne 0 ]; then
         echo "CMake build failed."
-        exit -0
+        exit 1
     else
         make
         if [ $? -ne 0 ]; then
             echo "Make failed."
-            exit -0
+            exit 1
         else
             ./app
         fi
-    fi        
+    fi
 }
 
 case "$OSTYPE" in
-  darwin*)  macos;; 
+  darwin*)  macos;;
   linux*)   linux;;
-  *)        echo "This app didn't support: $OSTYPE" ;;
+  *)        echo "This app doesn't support: $OSTYPE" ;;
 esac
